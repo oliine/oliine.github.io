2b0d1038bded74de0d3a9eb525363fad237962df
Caroline Palm <caarro@gmail.com>
2017-03-08 19:59:25 +0100

    inital commit

---
+++ generatename.js
@@ -0,0 +1,112 @@
+function generateSuperheroName (firstNameLetter, lastNameLetter) {
+	 var firstName;
+
+	 if (firstNameLetter === 'A') {
+	 	firstName = 'Wonder'
+	 	 } else if (firstNameLetter === 'B') {
+	 	 	firstName = 'Super'
+	 	 } else if (firstNameLetter === 'C') {
+	 	 	firstName = 'Cat'
+	 	 } else if (firstNameLetter === 'D') {
+	 	 	firstName = 'Wolf'
+	 	 } else if (firstNameLetter === 'E') {
+	 	 	firstName = 'Puma'
+	 	 } else if (firstNameLetter === 'F') {
+	 	 	firstName = 'Holy'
+	 	 } else if (firstNameLetter === 'G') {
+	 	 	firstName = 'Fat'
+	 	 } else if (firstNameLetter === 'H') {
+	 	  	firstName = 'Queen'
+	 	 } else if (firstNameLetter === 'I') {
+	 	 	firstName = 'Shadow'
+	 	 } else if (firstNameLetter === 'J') {
+	 	 	firstName = 'Uber'
+	 	 } else if (firstNameLetter === 'K') {
+	 	 	firstName = 'Qick'
+	 	 } else if (firstNameLetter === 'L') {
+	 	 	firstName = 'Rubber'
+	 	 } else if (firstNameLetter === 'M') {
+	 	 	firstName = 'Mystery'
+	 	 } else if (firstNameLetter === 'N') {
+	 	 	firstName = 'Chock'
+	 	 } else if (firstNameLetter === 'O') {
+	 	  	firstName = 'Tiny'
+	 	 } else if (firstNameLetter === 'P') {
+	 	 	firstName = 'Golden'
+	 	 } else if (firstNameLetter === 'Q') {
+	 	 	firstName = 'Silver'
+	 	 } else if (firstNameLetter === 'R') {
+	 	  	firstName = 'Bronze'
+	 	 } else if (firstNameLetter === 'S') {
+	 	 	firstName = 'The'
+	 	 } else if (firstNameLetter === 'T') {
+	 	 	firstName = 'Mighty'
+	 	 } else if (firstNameLetter === 'U') {
+	 	 	firstName = 'Smart'
+	 	 } else if (firstNameLetter === 'V') {
+	 	 	firstName = 'Wise'
+	 	 } else if (firstNameLetter === 'X') {
+	 	 	firstName = 'Hot'
+	 	 } else if (firstNameLetter === 'Y') {
+	 	 	firstName = 'Hilarious'
+	 	 } else if (firstNameLetter === 'Z') {
+	 	 	firstName = 'Goat'
+	 	 }
+	 	 var lastName;
+
+	 if (lastNameLetter === 'A') {
+	 	lastName = 'Butt' 
+	 } else if (lastNameLetter === 'B') {
+	 	 	lastName = 'Rider'
+	 	 } else if (lastNameLetter === 'B') {
+	 	 	lastName = 'Rider'
+	 	 } else if (lastNameLetter === 'C') {
+	 	 	lastName = 'Horse'
+	 	 } else if (lastNameLetter === 'D') {
+	 	 	lastName = 'Pyamas'
+	 	 } else if (lastNameLetter === 'E') {
+	 	 	lastName = 'Hiccup'
+	 	 } else if (lastNameLetter === 'F') {
+	 	 	lastName = 'Giraffe'
+	 	 }  else if (lastNameLetter === 'G') {
+	 	 	lastName = 'Bear'
+	 	 } else if (lastNameLetter === 'H') {
+	 	 	lastName = 'Stud'
+	 	 } else if (lastNameLetter === 'I') {
+	 	 	lastName = 'Idiot'
+	 	 } else if (lastNameLetter === 'J') {
+	 	 	lastName = 'Beast'
+	 	 } else if (lastNameLetter === 'K') {
+	 	 	lastName = 'Costume'
+	 	 } else if (lastNameLetter === 'L') {
+	 	 	lastName = 'Leather'
+	 	 } else if (lastNameLetter === 'M') {
+	 	 	lastName = 'Monster'
+	 	 } else if (lastNameLetter === 'N') {
+	 	 	lastName = 'Vilian'
+	 	 } else if (lastNameLetter === 'O') {
+	 	 	lastName = 'Gangster'
+	 	 } else if (lastNameLetter === 'P') {
+	 	 	lastName = 'Elephant'
+	 	 } else if (lastNameLetter === 'Q') {
+	 	 	lastName = 'Panther'
+	 	 } else if (lastNameLetter === 'R') {
+	 	 	lastName = 'Hero'
+	 	 } else if (lastNameLetter === 'S') {
+	 	 	lastName = 'Pink'
+	 	 } else if (lastNameLetter === 'T') {
+	 	 	lastName = 'Trousers'
+	 	 } else if (lastNameLetter === 'U') {
+	 	 	lastName = 'Outstanding'
+	 	 } else if (lastNameLetter === 'V') {
+	 	 	lastName = 'Lava'
+	 	 } else if (lastNameLetter === 'X') {
+	 	 	lastName = 'Vulcano'
+	 	 } else if (lastNameLetter === 'Y') {
+	 	 	lastName = 'Scream'
+	 	 } else if (lastNameLetter === 'Z') {
+	 	 	lastName = 'Macaroni'
+	 	 }
+
+	 return firstName + lastName;
+}
